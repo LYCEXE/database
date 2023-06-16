13d84f54c28fd36e0f632d2e4faab7fddb833f62
LYCEXE <51708133+LYCEXE@users.noreply.github.com>
2023-06-16 16:01:23 +0800

    Add files via upload

---
+++ create.h
@@ -0,0 +1,3 @@
+#include <string>
+using namespace std;
+int create_csv(string filename);
---
+++ database_project.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <create.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    create_csv("1.csv");
+}
+
+
